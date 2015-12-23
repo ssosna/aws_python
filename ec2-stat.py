a1fa8ef091d6f7d168c4422a5ee6bb34d30f9be6
ssosna <sergio.sosna@gmail.com>
2015-12-23 13:18:24 +0200

    Fourth review by SS. v2

--- ec2-stat.py
+++ ec2-stat.py
@@ -6,9 +6,9 @@ import boto.ec2.elb as elb
 from tabulate import tabulate
 from collections import namedtuple
 
-type_to_source = namedtuple('type_to_source', 'source function')
+type_to_source = namedtuple('type_to_source', 'source, function')
 
-""" Default source, resource and function map"""
+# Default sources, resources and functions map
 SOURCE_TO_TYPE = {
     'ec2': {
         'instance': 'get_only_instances(filters=filter_tags, instance_ids=aws_id)',
@@ -18,7 +18,8 @@ SOURCE_TO_TYPE = {
     'elb': {
         'elb': 'get_all_load_balancers(load_balancer_names=aws_id)'}
 }
-""" Table headers and data variable"""
+
+# Table headers and data variables
 PRINT_TABLE_HEADERS_LIST = [
     ['No', 'str(index)'],
     ['Resource ID', 'res.id'],
@@ -30,7 +31,7 @@ TYPE_TO_SOURCE = {
     type: type_to_source(source=source, function=function)
     for source in SOURCE_TO_TYPE
     for type, function in SOURCE_TO_TYPE[source].iteritems()
-}
+    }
 
 
 class awsResource:
@@ -40,26 +41,41 @@ class awsResource:
 
     @staticmethod
     def tags_parser(aws_tags):
-        # Parsing tags
+        # Parsing tags which received from command line
         aws_tags = ['tag:{}'.format(item) for item in aws_tags]
         filter_tags = [
             item.split('=')
             for item in aws_tags
-        ]
+            ]
         return dict(filter_tags)
 
     def get_resource_data(self, aws_id, aws_resource, aws_tags):
+        # Function gets data from AWS according to the query
+        # Output: list of resources data
+        output_data_table = list()
+        res_tags = str()
         filter_tags = self.tags_parser(aws_tags)
-        aws_query = 'self.ec2_connect.{0}'.format(aws_resource)
-        return eval(aws_query)
+        resource_data = eval('self.ec2_connect.{0}'.format(aws_resource))
+        for index, res in enumerate(resource_data):
+            table_string = []
+            print res.tags.items()
+            for tags_key, tags_value in res.tags.items():
+
+                res_tags += ' {}: {};'.format(tags_key, tags_value)
+            for headers_list_value in zip(*PRINT_TABLE_HEADERS_LIST)[1]:
+                table_string.append(eval(headers_list_value))
+            output_data_table.append(table_string)
+        print output_data_table
+        return output_data_table
 
 
 def print_result_tabulate(args):
     """
-    "args" variable contains arguments from command line:
+    Print function
+    :param args          : Variable contains arguments from command line:
     :param args.region   : AWS region
     :param args.id       : AWS resource ID
-    :param args.tags     : Users TAGs in list format
+    :param args.tags     : User's TAGs in list format
     :param args.resource : Selected AWS resource type from SOURCE_TO_TYPE dict
 
     :return: table output, for example:
@@ -69,18 +85,9 @@ def print_result_tabulate(args):
             |    1 | i-31d610e8    | AppVer: 1.2.2.2.5; Name: LAMP; | stopped |
             +------+---------------+--------------------------------+---------+
     """
-    output_table = list()
-    res_tags = str()
     table_headers = zip(*PRINT_TABLE_HEADERS_LIST)[0]
-    aws_resource_data = awsResource(args.region, TYPE_TO_SOURCE[args.resource].source).\
+    output_table = awsResource(args.region, TYPE_TO_SOURCE[args.resource].source). \
         get_resource_data(args.id, TYPE_TO_SOURCE[args.resource].function, args.tags)
-    for index, res in enumerate(aws_resource_data):
-        table_string = []
-        for tags_key, tags_value in res.tags.items():
-            res_tags += ' {}: {};'.format(tags_key, tags_value)
-        for headers_list_value in zip(*PRINT_TABLE_HEADERS_LIST)[1]:
-            table_string.append(eval(headers_list_value))
-        output_table.append(table_string)
     print tabulate(output_table, table_headers, tablefmt="grid")
 
 
